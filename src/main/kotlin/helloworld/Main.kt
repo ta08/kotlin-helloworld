f49888d4d794fdfe395129219327c8128b484566
ta08 <marl4dev@gmail.com>
2024-10-07 23:14:18 +0900

    remove the class

--- src/main/kotlin/helloworld/Main.kt
+++ src/main/kotlin/helloworld/Main.kt
@@ -1,7 +1,3 @@
-package org.example.helloworld
-
-class Main {
-    fun main(args: Array<String>) {
-        println("Hello World")
-    }
+fun main(args: Array<String>) {
+    println("Hello World")
 }
