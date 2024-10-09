e8236ff1ad89d9b1352b47dd741a26242440f2ef
ta08 <marl4dev@gmail.com>
2024-10-09 23:17:26 +0900

    add a comment

--- spring-helloworld/src/main/kotlin/com/example/spring_helloworld/HelloWorldController.kt
+++ spring-helloworld/src/main/kotlin/com/example/spring_helloworld/HelloWorldController.kt
@@ -3,6 +3,9 @@ package com.example.spring_helloworld
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * this controller can work... yeah!!!
+ */
 class HelloWorldController {
 
     @RestController
