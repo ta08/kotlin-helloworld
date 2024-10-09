c2bed52371660ec35f0d2b6ddbddd0b8c03885fc
ta08 <marl4dev@gmail.com>
2024-10-09 22:10:37 +0900

    2.2 Spring Boot で Hello World

---
+++ spring-helloworld/src/main/kotlin/com/example/spring_helloworld/HelloWorldController.kt
@@ -0,0 +1,15 @@
+package com.example.spring_helloworld
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+class HelloWorldController {
+
+    @RestController
+    class HelloWorldController {
+        @GetMapping("/")
+        fun helloWorld(): String {
+            return "Hello World"
+        }
+    }
+}
