e797d9c4b97270a7f39aaec96b28e7c790abb418
ta08 <marl4dev@gmail.com>
2024-10-09 23:17:16 +0900

    add db settings

---
+++ kotlin-spring-crud-web-api/src/main/kotlin/com/example/kotlin_spring_crud_web_api/HelloWorldController.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin_spring_crud_web_api
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloWorldController {
+    @GetMapping("/")
+    fun helloWorld(): String {
+        return "Hello World"
+    }
+}
