d176b205693281f5f3b364508e72e3bf9bc35a00
ta08 <marl4dev@gmail.com>
2024-10-12 13:07:37 +0900

    add a new project

---
+++ implementing-server-side-kotlin-development/settings.gradle.kts
@@ -0,0 +1,7 @@
+pluginManagement {
+	repositories {
+		maven { url = uri("https://repo.spring.io/snapshot") }
+		gradlePluginPortal()
+	}
+}
+rootProject.name = "implementing-server-side-kotlin-development"
